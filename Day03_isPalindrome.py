a8ed50eeacc11123a2dea290c1f6afd5727229a3
Shivaram1704 <53655030+Shivaram17@users.noreply.github.com>
2024-06-14 15:48:42 +0530

    Update Day03_isPalindrome.py

--- Day03_isPalindrome.py
+++ Day03_isPalindrome.py
@@ -1,10 +1,13 @@
 def is_Palindrome(str):
   len_str = len(str)
   c=0
-  for i in range(len(str)):
-    if str[i] == str[len(str)-i-1]:
+  for i in range(len_str/2):
+    if str[i] == str[len_str-i-1]:
       continue
     else:
       c=1
       break
+
+  # shortcut
+  print(str[::-1]==str)
   return c==0
